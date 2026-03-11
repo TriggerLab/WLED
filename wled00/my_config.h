f5d237fcb5677171dc01ee8acbee8c7938d54018
Trigger <2960051+TriggerLab@users.noreply.github.com>
2026-03-12 01:22:55 +0600

    Create my_config.h

---
+++ wled00/my_config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define WLED_USE_MY_CONFIG
+
+#define WLED_AP_SSID "Lumen-"
+#define WLED_AP_SSID_UNIQUE
+#define WLED_AP_PASS "A^hD7%k3iqwNax#5KC"
+
+#define WLED_DISABLE_ALEXA
+#define WLED_DISABLE_HUE
+#define WLED_DISABLE_INFRARED
